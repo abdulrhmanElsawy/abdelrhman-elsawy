4ff9683f9a43b85d0ae8fce572fd78853264d960
abdulrhmanElsawy <your.email@example.com>
2024-05-16 07:16:23 +0300

    Deployed website

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ useEffect(() => {
 
 const handleDownload = () => {
     // Replace the URL with the actual URL of the PDF file
-    const fileUrl = 'https://abdulrhmanelsawy.github.io/abdelrhman-elsawy/docs/Abdelrhman-Elsawy-CV.pdf';
+    const fileUrl = 'https://abdulrhmanelsawy.github.io/abdelrhman-elsawy/abdelrhman_Elsawy_Cv.pdf';
     window.open(fileUrl, '_blank');
 };
 

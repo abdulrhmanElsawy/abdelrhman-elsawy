6c9ddee5cd79c3e036f9803658417432ec2814ce
abdulrhmanElsawy <your.email@example.com>
2024-08-07 14:07:33 +0300

    first commit

--- components/en/Story.jsx
+++ components/en/Story.jsx
@@ -9,7 +9,7 @@ function Story() {
                 <div className='container'>
                     <div className='story-content'>
                         <p>
-                            I am a distinguished and enthusiastic web developer committed to achieving the goals of my clients. I offer exceptional and attractive solutions that leave a lasting impression. With diverse technical skills and a passion for developing user-friendly and beautifully designed websites, I strive to create outstanding online experiences for your business. I work with a team spirit and continuously seek innovation to ensure your website stands out from competitors and attracts customers. Contact me now to create a website that meets your clients' expectations and fosters the success of your business.
+                            I am a distinguished and enthusiastic web developer committed to achieving the goals of my clients. I offer exceptional and attractive solutions that leave a lasting impression. With diverse technical skills and a passion for developing user-friendly and beautifully designed websites, I strive to create outstanding online experiences for your business. I work with a team spirit and continuously seek innovation to ensure your website stands out from competitors and attracts customers. Contact me now to create a website that meets your clients&rsquo; expectations and fosters the success of your business.
                         </p>
                     </div>
                 </div>
